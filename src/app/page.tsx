fac5ab8b4bcbfbbb000d20809477adbca4a61857
semih <semihbl@icloud.com>
2026-02-13 17:07:27 +0300

    improvements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,4 @@
 import Hero3D from "@/components/hero/Hero3D"
-import QualitySection from "@/components/sections/QualitySection"
-import ImportSection from "@/components/sections/ImportSection"
-import ProductsSection from "@/components/sections/ProductsSection"
-import ContactSection from "@/components/sections/ContactSection"
 import TextileShowcase from "@/components/sections/TextileShowcase"
 
 export default function Home() {
